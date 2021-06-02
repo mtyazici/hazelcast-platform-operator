04176207dde9f27830c613bc609f475b4361db2e
hasancelik <hasan@hazelcast.com>
2021-06-02 10:07:47 +0300

    [CN-73] create Statefulset resource

--- controllers/hazelcast_controller.go
+++ controllers/hazelcast_controller.go
@@ -2,13 +2,18 @@ package controllers
 
 import (
 	"context"
-
+	"fmt"
 	"github.com/go-logr/logr"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	hazelcastcomv1alpha1 "github.com/hazelcast/hazelcast-enterprise-operator/api/v1alpha1"
+	hazelcastv1alpha1 "github.com/hazelcast/hazelcast-enterprise-operator/api/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
 )
 
 // HazelcastReconciler reconciles a Hazelcast object
@@ -22,26 +27,100 @@ type HazelcastReconciler struct {
 //+kubebuilder:rbac:groups=hazelcast.com,resources=hazelcasts/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=hazelcast.com,resources=hazelcasts/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Hazelcast object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *HazelcastReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = r.Log.WithValues("hazelcast", req.NamespacedName)
+	logger := r.Log.WithValues("hazelcast", req.NamespacedName)
 
-	// your logic here
+	h := &hazelcastv1alpha1.Hazelcast{}
+	err := r.Client.Get(ctx, req.NamespacedName, h)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			logger.Info("Hazelcast resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		logger.Error(err, "Failed to get Hazelcast")
+		return ctrl.Result{}, err
+	}
+
+	// Check if the statefulSet already exists, if not create a new one
+	found := &appsv1.StatefulSet{}
+	err = r.Get(ctx, types.NamespacedName{Name: h.Name, Namespace: h.Namespace}, found)
+	if err != nil && errors.IsNotFound(err) {
+		sts, err := r.statefulSetForHazelcast(h)
+		if err != nil {
+			logger.Error(err,"Failed to create new StatefulSet resource")
+			return ctrl.Result{}, err
+		}
+		logger.Info("Creating a new StatefulSet", "StatefulSet.Namespace", sts.Namespace, "StatefulSet.Name", sts.Name)
+		err = r.Create(ctx, sts)
+		if err != nil {
+			logger.Error(err, "Failed to create new StatefulSet", "StatefulSet.Namespace", sts.Namespace, "StatefulSet.Name", sts.Name)
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
+		logger.Error(err, "Failed to get StatefulSet")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
 func (r *HazelcastReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&hazelcastcomv1alpha1.Hazelcast{}).
+		For(&hazelcastv1alpha1.Hazelcast{}).
+		Owns(&appsv1.StatefulSet{}).
 		Complete(r)
+}
+
+
+// deploymentForMemcached returns a memcached Deployment object
+func (r *HazelcastReconciler) statefulSetForHazelcast(h *hazelcastv1alpha1.Hazelcast ) (*appsv1.StatefulSet, error) {
+	ls := labelsForHazelcast()
+	replicas := h.Spec.ClusterSize
+
+	sts := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      h.Name,
+			Namespace: h.Namespace,
+		},
+		Spec: appsv1.StatefulSetSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{{
+						Image:   ImageForCluster(h),
+						Name:    "hazelcast",
+						Ports: []v1.ContainerPort{{
+							ContainerPort: 5701,
+							Name:          "hazelcast",
+						}},
+					}},
+				},
+			},
+		},
+	}
+	// Set Hazelcast instance as the owner and controller
+	err := ctrl.SetControllerReference(h, sts, r.Scheme)
+	if err != nil {
+		return nil, err
+	}
+
+	return sts, nil
+}
+
+func labelsForHazelcast() map[string]string {
+	return map[string]string{
+		"app.kubernetes.io/name": "hazelcast",
+		"app.kubernetes.io/managed-by": "hazelcast-enterprise-operator",
+	}
+}
+
+func ImageForCluster(h *hazelcastv1alpha1.Hazelcast) string {
+	return fmt.Sprintf("%s:%s", h.Spec.Repository, h.Spec.Version)
 }
