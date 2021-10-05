b7213a2031d18dafb43419c1d64972acbc41435c
hasancelik <hasan@hazelcast.com>
2021-10-05 08:06:14 +0300

    increase go-test timeout for e2e tests

--- test/e2e/options.go
+++ test/e2e/options.go
@@ -13,6 +13,6 @@ var deleteTimeout time.Duration
 func init() {
 	flag.StringVar(&hzNamespace, "namespace", "hazelcast-enterprise-operator-system", "The namespace to run e2e tests")
 	flag.DurationVar(&interval, "interval", 1*time.Second, "The length of time between checks")
-	flag.DurationVar(&timeout, "test-timeout", 5*time.Minute, "Timeout for test steps")
+	flag.DurationVar(&timeout, "timeout", 5*time.Minute, "Timeout for test steps")
 	flag.DurationVar(&deleteTimeout, "delete-timeout", 5*time.Minute, "Timeout for resource deletions")
 }
