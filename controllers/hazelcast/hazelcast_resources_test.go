e5323aef062fdf6a9f287c3d1e56716fd06416f0
Serge Bishyr <seriybg@gmail.com>
2021-11-05 17:41:11 +0200

    fix unit tests

--- controllers/hazelcast/hazelcast_resources_test.go
+++ controllers/hazelcast/hazelcast_resources_test.go
@@ -26,55 +26,69 @@ func Test_mergeHazelcastSpecs(t *testing.T) {
 	}
 	tests := []struct {
 		name   string
-		target *hazelcastv1alpha1.HazelcastSpec
+		target hazelcastv1alpha1.HazelcastSpec
 		want   hazelcastv1alpha1.HazelcastSpec
 	}{
 		{
 			name:   "Empty hazelcast repository",
-			target: &hazelcastv1alpha1.HazelcastSpec{ClusterSize: n.DefaultClusterSize, Version: n.HazelcastVersion, LicenseKeySecret: n.LicenseKeySecret},
+			target: hazelcastv1alpha1.HazelcastSpec{ClusterSize: n.DefaultClusterSize, Version: n.HazelcastVersion, LicenseKeySecret: n.LicenseKeySecret},
 			want:   defaultHzSpec,
 		},
 		{
 			name:   "Empty hazelcast version",
-			target: &hazelcastv1alpha1.HazelcastSpec{ClusterSize: n.DefaultClusterSize, Repository: n.HazelcastRepo, LicenseKeySecret: n.LicenseKeySecret},
+			target: hazelcastv1alpha1.HazelcastSpec{ClusterSize: n.DefaultClusterSize, Repository: n.HazelcastRepo, LicenseKeySecret: n.LicenseKeySecret},
 			want:   defaultHzSpec,
 		},
 		{
 			name:   "Empty license key secret",
-			target: &hazelcastv1alpha1.HazelcastSpec{ClusterSize: n.DefaultClusterSize, Repository: n.HazelcastRepo, Version: n.HazelcastVersion},
+			target: hazelcastv1alpha1.HazelcastSpec{ClusterSize: n.DefaultClusterSize, Repository: n.HazelcastRepo, Version: n.HazelcastVersion},
 			want:   defaultHzSpec,
 		},
 		{
 			name:   "Empty cluster size",
-			target: &hazelcastv1alpha1.HazelcastSpec{LicenseKeySecret: n.LicenseKeySecret, Repository: n.HazelcastRepo, Version: n.HazelcastVersion},
+			target: hazelcastv1alpha1.HazelcastSpec{LicenseKeySecret: n.LicenseKeySecret, Repository: n.HazelcastRepo, Version: n.HazelcastVersion},
 			want:   defaultHzSpec,
 		},
 		{
 			name:   "Non empty hazelcast repository",
-			target: &hazelcastv1alpha1.HazelcastSpec{Repository: "myorg/hazelcast"},
+			target: hazelcastv1alpha1.HazelcastSpec{Repository: "myorg/hazelcast"},
 			want:   hazelcastv1alpha1.HazelcastSpec{Repository: "myorg/hazelcast", ClusterSize: n.DefaultClusterSize, Version: n.HazelcastVersion, LicenseKeySecret: n.LicenseKeySecret},
 		},
 		{
 			name:   "Non empty hazelcast version",
-			target: &hazelcastv1alpha1.HazelcastSpec{Version: "4.2"},
+			target: hazelcastv1alpha1.HazelcastSpec{Version: "4.2"},
 			want:   hazelcastv1alpha1.HazelcastSpec{Version: "4.2", ClusterSize: n.DefaultClusterSize, Repository: n.HazelcastRepo, LicenseKeySecret: n.LicenseKeySecret},
 		},
 		{
 			name:   "Non empty license key secret",
-			target: &hazelcastv1alpha1.HazelcastSpec{LicenseKeySecret: "license-key-secret"},
+			target: hazelcastv1alpha1.HazelcastSpec{LicenseKeySecret: "license-key-secret"},
 			want:   hazelcastv1alpha1.HazelcastSpec{LicenseKeySecret: "license-key-secret", ClusterSize: n.DefaultClusterSize, Repository: n.HazelcastRepo, Version: n.HazelcastVersion},
 		},
 		{
 			name:   "Non empty cluster size",
-			target: &hazelcastv1alpha1.HazelcastSpec{ClusterSize: 5},
+			target: hazelcastv1alpha1.HazelcastSpec{ClusterSize: 5},
 			want:   hazelcastv1alpha1.HazelcastSpec{ClusterSize: 5, LicenseKeySecret: n.LicenseKeySecret, Repository: n.HazelcastRepo, Version: n.HazelcastVersion},
 		},
 	}
+	scheme, _ := hazelcastv1alpha1.SchemeBuilder.Register(&hazelcastv1alpha1.Hazelcast{}, &hazelcastv1alpha1.HazelcastList{}).Build()
+	h := &hazelcastv1alpha1.Hazelcast{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "hazelcast",
+			Namespace: "default",
+		},
+	}
+	r := HazelcastReconciler{
+		Client: fake.NewClientBuilder().WithScheme(scheme).WithObjects(h).Build(),
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			applyDefaultHazelcastSpecs(tt.target)
-			if *tt.target != tt.want {
-				t.Errorf("HazelcastSpec = %v, want %v", *tt.target, tt.want)
+			h.Spec = tt.target
+			err := r.applyDefaultHazelcastSpecs(context.Background(), h)
+			if err != nil {
+				t.Errorf("Unexpected error have occured: %v", err)
+			}
+			if h.Spec != tt.want {
+				t.Errorf("HazelcastSpec = %v, want %v", tt.target, tt.want)
 			}
 		})
 	}
