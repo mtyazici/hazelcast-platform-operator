c41ab6b2b4ce20d11738bed3013e9234285453df
Muhammet Yazici <muhammet.yazici@hazelcast.com>
2021-12-07 13:59:14 +0300

    Update controller-manager check for e2e tests

--- test/e2e/managementcenter_controller_e2e_test.go
+++ test/e2e/managementcenter_controller_e2e_test.go
@@ -29,7 +29,7 @@ var _ = Describe("Management-Center", func() {
 	}
 
 	var controllerManagerName = types.NamespacedName{
-		Name:      "hazelcast-platform-controller-manager",
+		Name:      controllerManagerName(),
 		Namespace: hzNamespace,
 	}
 
