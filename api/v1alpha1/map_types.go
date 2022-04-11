019d274f77d1a2e178042e770b0aab6e69f721c4
Muhammet Yazici <muhammet.yazici@hazelcast.com>
2022-04-11 16:00:14 +0300

    Improve Map CR spec fields and comments

--- api/v1alpha1/map_types.go
+++ api/v1alpha1/map_types.go
@@ -6,34 +6,104 @@ import (
 
 // MapSpec defines the desired state of Map
 type MapSpec struct {
-	Name               string          `json:"name,omitempty"`
-	BackupCount        int             `json:"backupCount,omitempty"`
-	AsyncBackupCount   int             `json:"asyncBackupCount,omitempty"`
-	TimeToLiveSeconds  int             `json:"timeToLiveSeconds,omitempty"`
-	MaxIdleSeconds     int             `json:"maxIdleSeconds,omitempty"`
-	Eviction           *EvictionConfig `json:"eviction,omitempty"`
-	ReadBackupData     string          `json:"readBackupData,omitempty"`
-	Indexes            []IndexConfig   `json:"indexes,omitempty"`
-	PersistenceEnabled string          `json:"persistenceEnabled,omitempty"`
+	// Name of the map config to be created. If empty, CR name will be used.
+	// +optional
+	Name string `json:"name,omitempty"`
+
+	// Count of synchronous backups.
+	// +kubebuilder:default:=1
+	// +optional
+	BackupCount *int32 `json:"backupCount,omitempty"`
+
+	// Number of asynchronous backups. Unlike the synchronous backup process,
+	// asynchronous backup process does not block the map operations.
+	// +kubebuilder:default:=0
+	// +optional
+	AsyncBackupCount *int32 `json:"asyncBackupCount,omitempty"`
+
+	// Maximum time in seconds for each entry to stay in the map.
+	// If it is not 0, entries that are older than this time and not updated for this time are evicted automatically
+	// +kubebuilder:default:=0
+	// +optional
+	TimeToLiveSeconds *int32 `json:"timeToLiveSeconds,omitempty"`
+
+	// Maximum time in seconds for each entry to stay idle in the map.
+	// Entries that are idle for more than this time are evicted automatically.
+	// +kubebuilder:default:=0
+	// +optional
+	MaxIdleSeconds *int32 `json:"maxIdleSeconds,omitempty"`
+
+	// Configuration for removing data from the map when it reaches its max size.
+	// +optional
+	Eviction *EvictionConfig `json:"eviction,omitempty"`
+
+	// Used to enable reading from local backup map entries.
+	// It can be used if there is at least 1 sync or async backup.
+	// +kubebuilder:default:=false
+	// +optional
+	ReadBackupData bool `json:"readBackupData,omitempty"`
+
+	// Indexes to be created for the map data.
+	// You can learn more at https://docs.hazelcast.com/hazelcast/latest/query/indexing-maps
+	// +optional
+	Indexes []IndexConfig `json:"indexes,omitempty"`
+
+	// When enabled, map data will be persisted
+	// +kubebuilder:default:=false
+	// +optional
+	PersistenceEnabled bool `json:"persistenceEnabled,omitempty"`
 }
 
 type EvictionConfig struct {
+	// +kubebuilder:default:="NONE"
+	// +optional
 	EvictionPolicy string `json:"persistenceEnabled,omitempty"`
-	MaxSize        string `json:"maxSize,omitempty"`
-	MaxSizePolicy  string `json:"maxSizePolicy,omitempty"`
+
+	// +kubebuilder:default:=0
+	// +optional
+	MaxSize *int32 `json:"maxSize,omitempty"`
+
+	// +kubebuilder:default:="PER_NODE"
+	// +optional
+	MaxSizePolicy string `json:"maxSizePolicy,omitempty"`
 }
 
 type IndexConfig struct {
-	Name               string                    `json:"name,omitempty"`
-	Type               string                    `json:"type,omitempty"`
-	Attributes         string                    `json:"attributes,omitempty"`
+	// Name of the index config, optional.
+	// +optional
+	Name string `json:"name,omitempty"`
+
+	Type IndexType `json:"type"`
+
+	Attributes []string `json:"attributes"`
+
+	// +optional
 	BitMapIndexOptions *BitMapIndexOptionsConfig `json:"bitMapIndexOptions,omitempty"`
 }
 
+// +kubebuilder:validation:Enum=SORTED;HASH;BITMAP
+type IndexType string
+
+const (
+	IndexTypeSorted IndexType = "SORTED"
+	IndexTypeHash   IndexType = "HASH"
+	IndexTypeBitmap IndexType = "BITMAP"
+)
+
 type BitMapIndexOptionsConfig struct {
-	UniqueKey           string `json:"uniqueKey,omitempty"`
-	UniqueKeyTransition string `json:"uniqueKeyTransition,omitempty"`
+	UniqueKey string `json:"uniqueKey"`
+
+	UniqueKeyTransition UniqueKeyTransition `json:"uniqueKeyTransition"`
 }
+
+// +kubebuilder:validation:Enum=OBJECT;LONG;RAW
+type UniqueKeyTransition string
+
+const (
+	UniqueKeyTransitionObject UniqueKeyTransition = "OBJECT"
+	UniqueKeyTransitionLong   UniqueKeyTransition = "LONG"
+	UniqueKeyTransitionRAW    UniqueKeyTransition = "RAW"
+)
 
 // MapStatus defines the observed state of Map
 type MapStatus struct {
