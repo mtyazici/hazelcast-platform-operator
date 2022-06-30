9550db50bf284ea7c1e468b95c625976cc404d57
AB <alexander.borschuk@hazelcast.com>
2022-06-30 11:14:55 +0300

    Updated release workflow (#266)
    * Update release workflow
    [no ci]

--- .github/scripts/publish-rhel.sh
+++ .github/scripts/publish-rhel.sh
@@ -131,3 +131,43 @@ wait_for_container_publish()
         sleep 120
     done
 }
+
+checking_image_grade()
+{
+    local PROJECT_ID=$1
+    local VERSION=$2
+    local RHEL_API_KEY=$3
+    local TIMEOUT_IN_MINS=$4
+    FILTER="filter=deleted==false;repositories.published==false;repositories.tags.name==${VERSION}"
+    INCLUDE="include=data.freshness_grades.grade&include=data.freshness_grades.end_date"
+
+    local NOF_RETRIES=$(( $TIMEOUT_IN_MINS * 3 ))
+    for i in `seq 1 ${NOF_RETRIES}`; do
+
+    local GRADE_PRESENCE=$(curl -s -X 'GET' \
+      "https://catalog.redhat.com/api/containers/v1/projects/certification/id/${PROJECT_ID}/images?${FILTER}&page_size=100&page=0" \
+      -H "accept: application/json" \
+      -H "X-API-KEY: ${RHEL_API_KEY}" | jq -r -e '.data[0].freshness_grades | length')
+
+        if [[ ${GRADE_PRESENCE} -ne "0" ]]; then
+            GRADE_A=$(curl -s -X 'GET' \
+            "https://catalog.redhat.com/api/containers/v1/projects/certification/id/${PROJECT_ID}/images?${INCLUDE}&${FILTER}&page_size=100&page=0" \
+            -H "accept: application/json" \
+            -H "X-API-KEY: ${RHEL_API_KEY}" | jq -e '.data[0].freshness_grades[] | select(.grade =="A") | length')
+        if [[ ${GRADE_A} -ne "0" ]]; then
+            echo "The submitted image got a Health Index 'A'."
+            return 0
+        else
+            echo "The submitted image didn’t get a Health Index 'A'."
+            exit 1
+        fi
+        else
+            echo "The submitted image still has unknown Health Index Image, waiting..."
+        fi
+        if [[ ${i} == ${NOF_RETRIES} ]]; then
+            echo "Timeout! The submitted image has 'Unknown' Health Index."
+            return 42
+        fi
+        sleep 20
+    done
+}
