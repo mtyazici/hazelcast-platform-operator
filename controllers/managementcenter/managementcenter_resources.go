18806bb80ec1159da0978c0b8f1471dc6f189387
Mizbah Celik <108626494+mizbahcelik@users.noreply.github.com>
2022-08-26 10:16:40 +0300

    [CN-400] skip lock check for mc (#333)

--- controllers/managementcenter/managementcenter_resources.go
+++ controllers/managementcenter/managementcenter_resources.go
@@ -371,7 +371,7 @@ func env(mc *hazelcastv1alpha1.ManagementCenter) []v1.EnvVar {
 			v1.EnvVar{
 				Name: javaOpts,
 				Value: fmt.Sprintf("-Dhazelcast.mc.license=$(MC_LICENSE_KEY) -Dhazelcast.mc.healthCheck.enable=true"+
-					" -Dhazelcast.mc.tls.enabled=false -Dmancenter.ssl=false -Dhazelcast.mc.phone.home.enabled=%t", util.IsPhoneHomeEnabled()),
+					" -Dhazelcast.mc.lock.skip=true -Dhazelcast.mc.tls.enabled=false -Dmancenter.ssl=false -Dhazelcast.mc.phone.home.enabled=%t", util.IsPhoneHomeEnabled()),
 			},
 		)
 	} else {
@@ -379,7 +379,7 @@ func env(mc *hazelcastv1alpha1.ManagementCenter) []v1.EnvVar {
 			v1.EnvVar{
 				Name: javaOpts,
 				Value: fmt.Sprintf("-Dhazelcast.mc.healthCheck.enable=true -Dhazelcast.mc.tls.enabled=false -Dmancenter.ssl=false"+
-					" -Dhazelcast.mc.phone.home.enabled=%t", util.IsPhoneHomeEnabled()),
+					" -Dhazelcast.mc.lock.skip=true -Dhazelcast.mc.phone.home.enabled=%t", util.IsPhoneHomeEnabled()),
 			},
 		)
 	}
